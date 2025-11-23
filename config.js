98b0534a8f98d64ac0658f02b572e8d4133a5b5f
Shehzad Ahmed <ffshehzadffff@outlook.com>
2025-11-23 14:24:05 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    WEBHOOK_URL: "https://discord.com/api/webhooks/1435143642741342219/qrH9-UqI_he3FLKErGBtEFpnREK99HbMdBXOLh3W1U_xRYK8leRc-Dr5ZO8x2IMA4n1E",
+    REDIRECT_URL: "https://shop.playoxide.com/" 
+};
